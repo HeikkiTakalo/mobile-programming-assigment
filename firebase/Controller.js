4f73f0efb740ab93ec7d3177e57e663fb71cc171
HeikkiTakalo <heikkitakalo@windowslive.com>
2025-03-11 16:37:14 +0200

    controller made

---
+++ firebase/Controller.js
@@ -0,0 +1,33 @@
+import { addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, query, updateDoc, } from "firebase/firestore";
+import {useEffect, useState} from "react";
+import { db, LOCATIONS_REF } from "./config";
+
+
+export function useFireLocations(){
+    const [locations, setLocations] = useState([])
+
+    useEffect(()=>{
+
+
+        const q = query(collection(db, LOCATIONS_REF));
+
+        onSnapshot(q, querySnaphot => {
+            setLocations( querySnaphot.docs.map(doc => {
+                return { id: doc.id, ...doc.data() }
+            }));
+        });
+
+
+    }, []);
+
+
+    return locations;
+}
+
+export function addLocation(name, description, rating) {
+    return addDoc(collection(db, LOCATIONS_REF), {
+        name,
+        description,
+        rating
+    }).catch(error => console.log(error.message));
+}
